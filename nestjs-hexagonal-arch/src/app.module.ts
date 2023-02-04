276b8aa7a1e845593e4f3a577a6383f499b4f944
Gregory Victorelli <gregory@gvictorelli.dev>
2023-02-04 11:13:50 -0300

    nestjs arquitetura hexagonal

---
+++ nestjs-hexagonal-arch/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { List } from './lists/entities/list.entity';
+import { ListsModule } from './lists/lists.module';
+
+@Module({
+  imports: [
+    ListsModule,
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: ":memory:",
+      autoLoadModels: true,
+      models: [List]
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ nestjs-hexagonal-arch/src/lists/lists.service.ts
@@ -0,0 +1,48 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { lastValueFrom } from 'rxjs';
+import { CreateListDto } from './dto/create-list.dto';
+import { UpdateListDto } from './dto/update-list.dto';
+import { List } from './entities/list.entity';
+
+@Injectable()
+export class ListsService {
+  
+  constructor(
+    @InjectModel(List)
+    private listModel: typeof List,
+    private httpService: HttpService
+  ){
+    
+  }
+
+  async create(createListDto: CreateListDto) {
+    const list = await this.listModel.create(createListDto);
+    await lastValueFrom(this.httpService.post('lists', { 
+        name: list.name, 
+      }),
+    );
+    return list;
+  }
+
+  findAll() {
+    return this.listModel.findAll();
+  }
+
+  async findOne(id: number) {
+    const list =  await this.listModel.findByPk(id);
+    if(!list){
+      throw new Error('List not found')
+    }
+      return list;
+  }
+
+  update(id: number, updateListDto: UpdateListDto) {
+    return `This action updates a #${id} list`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} list`;
+  }
+}
