b67872742c6ce08b6faf7316a66a02c4a7aa5bac
Gregory V. Soares <gregory@gregory>
2023-02-05 19:03:00 -0300

    nestjs arquitetura hexagonal - implementacao gateway

--- nestjs-hexagonal-arch/src/lists/lists.service.spec.ts
+++ nestjs-hexagonal-arch/src/lists/lists.service.spec.ts
@@ -1,18 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ListModel } from './entities/list.model';
 import { ListsService } from './lists.service';
+
+const mockList = {
+  create: jest
+  .fn()
+  .mockReturnValue(Promise.resolve(new ListModel({ name: 'my list'}))),
+};
+
+const mockHttpService = {
+  post: jest.fn(),
+};
 
 describe('ListsService', () => {
   let service: ListsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ListsService],
-    }).compile();
-
-    service = module.get<ListsService>(ListsService);
+  beforeEach(() =>  {
+    service = new ListsService(mockList as any, mockHttpService as any);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('deve criar uma lista', async() => {
+    const list = await service.create({ name: 'my list' });
+    console.log(list);
   });
+  // beforeEach(async () => {
+  //   const module: TestingModule = await Test.createTestingModule({
+  //     providers: [ListsService],
+  //   }).compile();
+
+  //   service = module.get<ListsService>(ListsService);
+  // });
+
+  // it('should be defined', () => {
+  //   expect(service).toBeDefined();
+  // });
 });
